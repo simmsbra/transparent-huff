11092835ddca395849c8902d475cdd667570a2d1
Brandon Simmons <simmsbra@gmail.com>
2022-08-23 10:54:58 -0500

    Removed disused compilation option "-lm"

--- build.sh
+++ build.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-# compile with debugging symbols, a lot of warnings, and the C17 standard. also,
-# link <math.h>
+# compile with debugging symbols, a lot of warnings, and the C17 standard
 gcc -g -Wall -Wextra -std=c17 \
     src/bitbuffer.c src/huffman_tree.c src/encoder.c \
-    -lm \
     -o encoder
 
 gcc -g -Wall -Wextra -std=c17 \
     src/bitbuffer.c src/huffman_tree.c src/decoder.c \
-    -lm \
     -o decoder
