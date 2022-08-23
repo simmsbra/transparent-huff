807735f315e3a5ebdfffd62c821da62387bc5227
Brandon Simmons <simmsbra@gmail.com>
2022-08-23 11:09:39 -0500

    Refactor: Moved filenames to variables in helper script

--- compress-then-decompress.sh
+++ compress-then-decompress.sh
@@ -6,45 +6,48 @@ if [ "$1" == "" ]; then
     exit 1
 fi
 
-if [ -f "$1".thf ]; then
-    echo "Error: Encoder output file $1.thf already exists."
+COMPRESSED_FILE_NAME="$1.thf"
+DECOMPRESSED_FILE_NAME="$1.thf.reversed"
+
+if [ -f "$COMPRESSED_FILE_NAME" ]; then
+    echo "Error: Encoder output file $COMPRESSED_FILE_NAME already exists."
     exit 1
 fi
-if [ -f "$1".thf.reversed ]; then
-    echo "Error: Decoder output file $1.thf.reversed already exists."
+if [ -f "$DECOMPRESSED_FILE_NAME" ]; then
+    echo "Error: Decoder output file $DECOMPRESSED_FILE_NAME already exists."
     exit 1
 fi
 
-if ! ./encoder "$1" > "$1".thf ; then
+if ! ./encoder "$1" > "$COMPRESSED_FILE_NAME" ; then
     echo "Error: Encoder failed."
     echo "You can now delete the following file that was created:"
-    echo "      compressed file: \"$1.thf\""
+    echo "      compressed file: \"$COMPRESSED_FILE_NAME\""
     exit 1
 fi
 
-if ! ./decoder "$1".thf > "$1".thf.reversed ; then
+if ! ./decoder "$COMPRESSED_FILE_NAME" > "$DECOMPRESSED_FILE_NAME" ; then
     echo "Error: Decoder failed."
     echo "You can now delete the following files that were created:"
-    echo "      compressed file: \"$1.thf\""
-    echo "    decompressed file: \"$1.thf.reversed\""
+    echo "      compressed file: \"$COMPRESSED_FILE_NAME\""
+    echo "    decompressed file: \"$DECOMPRESSED_FILE_NAME\""
     exit 1
 fi
 
 # using "<" prevents wc from printing the filename after the number of bytes
-FILE_ORIGINAL_SIZE=$(wc --bytes < "$1")
-FILE_COMPRESSED_SIZE=$(wc --bytes < "$1".thf)
+ORIGINAL_FILE_SIZE=$(wc --bytes < "$1")
+COMPRESSED_FILE_SIZE=$(wc --bytes < "$COMPRESSED_FILE_NAME")
 # this is only integer division, but that's ok for our purposes
-PERCENTAGE=$((100 * $FILE_COMPRESSED_SIZE / $FILE_ORIGINAL_SIZE))
+PERCENTAGE=$((100 * $COMPRESSED_FILE_SIZE / $ORIGINAL_FILE_SIZE))
 echo "Results:"
 echo "The compressed file is $PERCENTAGE% the size of the original file."
 
-if ! diff "$1" "$1".thf.reversed ; then
+if ! diff "$1" "$DECOMPRESSED_FILE_NAME" ; then
     echo "Error: The original file and the decompressed file differ."
     exit 1
 else
     echo "The original file and the decompressed file match."
     echo "You can now delete the following files that were created:"
-    echo "      compressed file: \"$1.thf\""
-    echo "    decompressed file: \"$1.thf.reversed\""
+    echo "      compressed file: \"$COMPRESSED_FILE_NAME\""
+    echo "    decompressed file: \"$DECOMPRESSED_FILE_NAME\""
     exit 0
 fi
